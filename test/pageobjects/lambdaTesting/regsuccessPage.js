ae159fb234a1a739404e6c1d5d347a85e2266aa0
nikhil-1100 <optimalaces.2001@gmail.com>
2024-07-16 17:23:48 +0530

    These folder include the assignment on 12//7/24

---
+++ test/pageobjects/lambdaTesting/regsuccessPage.js
@@ -0,0 +1,16 @@
+import commonPage from "./commonPage.js";
+
+class regsuccessPage extends commonPage {
+    constructor() {
+        super();
+        this.$header=()=> $(`//i[@class="fas fa-check-circle text-success"]`)
+        this.$continueButton=()=> $(`//a[normalize-space()="Continue"]`)
+    }
+
+    async clickContinue() {
+        await this.$continueButton().waitForDisplayed({timeout: 10000, timeoutMsg: "Continue button is not visible"});
+        await this.$continueButton().click();
+    }
+
+}
+export default new regsuccessPage()
